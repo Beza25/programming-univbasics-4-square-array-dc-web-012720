b22381c6845d5bd4b4bd4d7f0c0e22a7b3c06bec
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-03 23:17:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  i= 0
+  new_numbers =[]
+ while i < numbers.length
+  do
+    new_numbers. push (numbers[i] * numbers[i])
+    i += 1 
+  end
+  puts new_numbers
+   
 end
+
